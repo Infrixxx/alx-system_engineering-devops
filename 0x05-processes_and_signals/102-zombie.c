b12e893eaf8cfab24c1087af95faf98591ad4bf9
Boitumelo <120354783+Infrixxx@users.noreply.github.com>
2023-10-28 04:23:49 +0200

    Updated zombie processes

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -1,38 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
-int infinite_while(void)
-{
-    while (1)
-    {
-        sleep(1);
-    }
-    return (0);
-}
-
+/**
+ * main - zombies
+ *
+ * Description: make five zombies
+ * Return: 0 for success
+ */
 int main(void)
 {
-    pid_t child_pid;
-    int i;
+	int i;
+	pid_t pidme;
 
-    for (i = 0; i < 5; i++)
-    {
-        child_pid = fork();
-        if (child_pid == -1)
-        {
-            perror("Fork failed");
-            exit(1);
-        }
-        if (child_pid == 0)
-        {
-            // This is the child process
-            printf("Zombie process created, PID: %d\n", getpid());
-            exit(0);
-        }
-    }
-
-    infinite_while();
-
-    return (0);
+	i = 0;
+	while (i < 5)
+	{
+		pidme = fork();
+		if (pidme)
+			printf("Zombie process created, PID: %i\n", pidme);
+		else
+			exit(0);
+		i++;
+	}
+	sleep(100);
+	return (0);
 }
