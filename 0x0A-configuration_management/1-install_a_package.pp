bf8601011bb3910e30e8a5a47e1a1b1896aa8718
Boitumelo <120354783+Infrixxx@users.noreply.github.com>
2023-12-23 01:05:59 +0200

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,12 +1,23 @@
-# Install flask
+package { 'python3.8.10':  # Specify exact Python version
+  ensure => present,
+  before => Package['python3-pip'],  # Install Python before pip3
+}
+
 package { 'python3-pip':
   ensure => present,
-  before => Package['Flask'],  # Ensure pip3 is installed first
+  before => Package['Flask'],
 }
 
 package { 'Flask':
   ensure          => '2.1.0',
   provider        => 'pip3',
   install_options => ['--upgrade'],
-  require         => Package['python3-pip'],  # Require pip3
+  require         => Package['python3-pip'],
+}
+
+package { 'Werkzeug':
+  ensure          => '2.1.1',
+  provider        => 'pip3',
+  install_options => ['--upgrade'],
+  require         => Package['python3-pip'],
 }
