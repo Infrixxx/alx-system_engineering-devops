20d595440f89f6eeb20b390627e2fe7ce61f1e5f
Boitumelo <120354783+Infrixxx@users.noreply.github.com>
2023-12-23 00:52:13 +0200

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,9 +1,5 @@
-package { 'python3-pip':
-  ensure => present,  # Ensure pip3 is installed
-}
-
-package { 'Flask':
-  ensure          => '2.1.0',       # Specify exact version
-  provider        => 'pip3',        # Use pip3 for installation
-  install_options => ['--upgrade'], # Force upgrade if needed
+# Install flask
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
