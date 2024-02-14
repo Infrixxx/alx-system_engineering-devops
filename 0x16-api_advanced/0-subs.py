adaf72bb4823547674bcc2fd2fba828fc0879a70
Boitumelo <120354783+Infrixxx@users.noreply.github.com>
2024-02-14 22:03:45 +0200

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,7 +10,7 @@ def number_of_subscribers(subreddit):
     """ Returns the number of subscribers for a given subreddit. """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, allow_redirects=False, headers=headers)
+    response = requests.get(url, headers=headers)
 
     if response.status_code == 200:
         data = response.json()
